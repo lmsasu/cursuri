d35657fcd1238dae632723cd5e87ed43a3f43c08
Lucian Sasu <lmsasu@yahoo.com>
2019-10-19 14:43:08 +0300

    Lab 3 ASSE.NET added

---
+++ ASSE.NET/laboratoare/laborator3/cod/DemoMEL_Logging/DemoMEL_Logging/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoMEL_Logging
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Logger.IsLoggingEnabled())
+            {
+                LogEntry logEntry = new LogEntry()
+                {
+                    EventId = 100,
+                    Priority = 2,
+                    Message = "mesaj de informare",
+                    Severity = System.Diagnostics.TraceEventType.Information
+                };
+                logEntry.Categories.Add("Trace");
+                logEntry.Categories.Add("UI Events");
+
+                logEntry.ExtendedProperties["locatie"] = this.GetType().ToString();
+
+                Logger.Write(logEntry);
+            }
+
+        }
+    }
+}
---
+++ ASSE.NET/laboratoare/laborator3/cod/DemoMEL_Logging/DemoMEL_Logging/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoMEL_Logging
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Logger.SetLogWriter(new LogWriterFactory().Create());
+            Application.Run(new Form1());
+        }
+    }
+}
